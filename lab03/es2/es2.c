316088212d92ab551d8485b3fcc3ae371d96a23a
Barberis Enrico <enbarberis@gmail.com>
2015-11-01 19:36:26 +0100

    fixed lab03 es02

--- lab03/es2/es2.c
+++ lab03/es2/es2.c
@@ -4,37 +4,75 @@
 
 void print_error(char *msg, int exit_value);
 
+void create_children(int n);
+
 int main(int argc, char **argv)
 {
-	int n;		//per quante volte devo duplicare un processo
-	int t;		//quanto deve attendere in secondi le foglie
-	int i;		//var. per ciclo for
+	int n;
+	int t;
 
 	if(argc < 3)
 	{
-		print_error("Parameters error!\nUsage: ./es1 <n> <t>\n", -1);
+		print_error("Parameters error!\nUsage: ./es2 <n> <t>\n", -1);
 	}
 
 	sscanf(argv[1], "%d", &n);
 	sscanf(argv[2], "%d", &t);
 
-	for(i=0; i<n; i++)
-	{
-		if(fork() < 0)
-		{
-			print_error("Can't fork\n", -2);
-		}
-		
-		if(i == n-1)
-		{
-			sleep(t);
-			printf("Process %d is terminated\n", getpid());
-		}
-	}
+	create_children(n);
+
+	printf("\t\t[%d->%d]I'm leaf process running\n", getppid(), getpid());
+	sleep(t);
 
 	return 0;
 }
 
+
+void create_children(int n)
+{
+	int pid1;
+	int pid2;
+
+	if(n > 0)
+	{
+		pid1 = fork();
+		if(pid1 < 0)
+		{
+			print_error("Can't fork!\n", -2);
+		}
+		else if(!pid1)
+		{
+			//child
+			printf("[%d->%d]I'm going to duplicate myself\n", getppid(), getpid());
+			
+			pid2 = fork();
+
+			if(pid2 < 0)
+			{
+				print_error("Can't fork!\n", -3);
+			}
+			else if(!pid2)
+			{
+				//child of child	
+				printf("[%d->%d]I'm child of child\n", getppid(), getpid());
+				create_children(n-1);
+			}
+			else
+			{
+				//child
+				printf("[%d->%d]I'm child\n", getppid(), getpid());
+				create_children(n-1);
+			}
+		}
+		else
+		{
+			//father
+			printf("[%d->%d]I'm father and im going to die\n", getppid(), getpid());
+			exit(0);
+		}
+	}
+
+}
 
 void print_error(char *msg, int exit_value)
 {
