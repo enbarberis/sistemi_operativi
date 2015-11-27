94b5fb5350973403f9ea666d8178a21de316d303
Barberis Enrico <enbarberis@gmail.com>
2015-11-27 19:18:28 +0100

    lab07es1 first solution

---
+++ lab07/es1/concurrent/es1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int n;
+	int *v;
+	int i;
+	int pid;
+
+	//read parameter
+	if(argc < 2)
+	{
+		fprintf(stderr, "Parameters error!\nUsage: ./es1 <n>\n");
+		return 1;
+	}
+
+	sscanf(argv[1], "%d", &n);
+
+	
+	//allocate array
+	v = (int *) malloc(n * sizeof(int));
+	if(v == NULL)
+	{
+		fprintf(stderr, "Can't malloc!\n");
+		return 2;
+	}
+
+
+	//create a binary tree of fork
+	for(i=0; i<n; i++)
+	{
+		pid = fork();
+		
+		if(pid < 0)
+		{
+			fprintf(stderr, "Can't fork!\n");
+			return 3;
+		}
+		else if(!pid)
+		{
+			//CHILD
+			v[i] = 1;
+		}
+		else
+		{
+			//FATHER
+			v[i] = 0;
+		}
+	}
+
+	printf("[%d]: ", getpid());
+	for(i=0; i<n; i++)
+	{
+		printf("%d", v[i]);
+	}
+
+	printf("\n");
+	
+	return 0;
+}
